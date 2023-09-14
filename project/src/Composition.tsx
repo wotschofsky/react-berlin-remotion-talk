397d8d3aca8be5ee22e1f88db3c07394db0e2410
Felix Wotschofsky <felix@wotschofsky.com>
2023-09-14 21:12:44 +0200

    Step 4: Sequences

--- project/src/Composition.tsx
+++ project/src/Composition.tsx
@@ -1,14 +1,22 @@
-import { AbsoluteFill } from 'remotion';
+import { AbsoluteFill, Series } from 'remotion';
 import { loadFont } from '@remotion/google-fonts/Inter';
 import type { FC } from 'react';
 
+import Bridge from './components/Bridge';
 import Intro from './components/Intro';
 
 const { fontFamily: interFont } = loadFont();
 
 const Composition: FC = () => (
   <AbsoluteFill className="bg-slate-900" style={{ fontFamily: interFont }}>
-    <Intro />
+    <Series>
+      <Series.Sequence durationInFrames={50}>
+        <Intro />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={40}>
+        <Bridge />
+      </Series.Sequence>
+    </Series>
   </AbsoluteFill>
 );
 
---
+++ project/src/components/Bridge.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react';
+import { AbsoluteFill } from 'remotion';
+
+const Bridge: FC = () => (
+  <AbsoluteFill className="flex justify-center items-center">
+    <h2 className="text-white text-8xl text-center">
+      Here's what people
+      <br />
+      had to say
+    </h2>
+  </AbsoluteFill>
+);
+
+export default Bridge;
