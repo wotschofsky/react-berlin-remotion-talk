4e61a0072011bfeca802dc2dcc169b00cda041ea
Felix Wotschofsky <felix@wotschofsky.com>
2023-09-29 13:04:33 +0200

    Handle special case for next meetup date

--- project/src/components/Outro.tsx
+++ project/src/components/Outro.tsx
@@ -3,11 +3,24 @@ import { AbsoluteFill } from 'remotion';
 
 const getNextDate = () => {
   const now = new Date();
-  const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1); // Get the first day of the next month
-  const lastDayOfNextMonth = new Date(nextMonth.getFullYear(), nextMonth.getMonth() + 1, 0); // Get the last day of the next month
+  const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+  const lastDayOfNextMonth = new Date(
+    nextMonth.getFullYear(),
+    nextMonth.getMonth() + 1,
+    0,
+  );
 
   const date = new Date(lastDayOfNextMonth);
   while (date.getDay() !== 4) {
+    date.setDate(date.getDate() - 1);
+  }
+
+  // Special one-time change in pattern
+  if (
+    date.getFullYear() === 2023 &&
+    date.getMonth() === 9 &&
+    date.getDate() === 26
+  ) {
     date.setDate(date.getDate() - 1);
   }
 
